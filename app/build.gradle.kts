fff16eca05338b62e609819bc11a452d17d2f60c
jimmy90109 <85556680+jimmy90109@users.noreply.github.com>
2026-01-21 23:05:55 +0800

    chore: update versionName to 1.0.0-beta.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.github.jimmy90109.geoalarm"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2601190
-        versionName = "1.0.0-beta.11"
+        versionCode = 2601210
+        versionName = "1.0.0-beta.12"
         
         manifestPlaceholders["GOOGLE_MAPS_API_KEY"] = localProperties.getProperty("maps.apiKey") ?: ""
         manifestPlaceholders["appName"] = "@string/app_name"
